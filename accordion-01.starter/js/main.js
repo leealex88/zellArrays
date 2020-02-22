c688c197c79e4a0020bdacae3c17ee43805c50f7
ola <olasvoice@gmail.com>
2020-02-22 15:10:10 +0000

    .
    .

--- accordion-01.starter/js/main.js
+++ accordion-01.starter/js/main.js
@@ -1,1 +1,29 @@
-// Start writing JavaScript here!
+const accordions = Array.from(document.querySelectorAll('.accordion'))
+const chooseButton = document.querySelector('.button')
+chooseButton.style.setProperty('background', 'red')
+
+
+const element = document.querySelector('.element')
+const style = getComputedStyle(element)
+
+const backgroundColor = style.backgroundColor
+// console.log(backgroundColor)
+
+// console.log(style)
+
+
+
+accordions.forEach(accordion => {
+    const accordionHeader = accordion.querySelector('.accordion__header')
+
+    accordionHeader.addEventListener('click', event => {
+        accordion.classList.toggle('is-open')
+    })
+})
+
+
+
+
+const list = document.querySelectorAll('li')
+const firstItem = list[0]
+console.log(firstItem)
